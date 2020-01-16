91ddb77cd84b2743f25c08bf917c4fd6e5306232
katychuang <katychuang@gmail.com>
2020-01-16 12:59:04 -0500

    drafting up an example quickly. might edit later

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# assignment-template
-A example for organizing your repository when submitting assignments
+# Title of your project
+
+Short blurb of your project. This repository was created to demonstrate to students an example for organizing your repository when submitting assignments. Take the advice provided here as a guideline.
+
+Longer descriptions of your project can be helpful to include. Writing about your project can cover the 5 w's: who, what, when, where, why and also how.
+
+Author notes. This repository was created by Professor Katherine Chuang for use with teaching  at Brooklyn College's Department of CIS.
+
+# Installation
+
+When "shipping your code", you may be sending your code off to a teammate in addition to the end user. It helps to think in their shoes. What do they need to be able to compile your code? What file(s) should be compiled and how does one do that?
+
+## Dependencies
+
+* [Java 8](https://docs.oracle.com/javase/8/docs/api/index.html)
+* Terminal for command line access. I use [iterm2 for macos](https://iterm2.com/)
+* [Travis CI](https://travis-ci.com/)
+
+While a short list of the dependencies is often sufficient, sometimes it helps to elaborate in detail the rationale behind selecting certain dependencies. Java 8 is used here because it's the department's officially supported language and version.
+
+It also helps to describe the connections between dependencies. This repository is linked to Travis-CI by way of a `.travis.yml` file in the root of the directory.
+
+## Setup
+
+These are the steps to compile `Main.java` and run the program `Main`. Source code is found in the src folder. These steps are for use with command line workflow such as with a terminal.
+
+1. Sign up for a GitHub account if you don't have one. Set up your SSH keys with github.
+2. Clone this repository `git clone git@github.com:cisc3130-s20/assignment-template.git` to your machine to get a copy.
+3. Move into the project's root directory with `cd assignment-template` [^1]
+4. Move again into the subdirectory `src` using the command `cd src`
+5. Compile the java file using the java compiler command `javac Main.java`
+6. Run the compiled java program with the java command `java Main`
+
+You should see an output similar to the following:
+
+```sh
+$ javac Main.java
+$ java Main
+Hello World
+```
+
+## Folder Structure
+
+Code is saved into the `src` folder.
+
+
+[^1]: the command `cd` refers to the action of [change directory](https://en.wikipedia.org/wiki/Cd_(command)) and is an easier typed abbreviation :)
---
+++ src/Main.java
@@ -0,0 +1,97 @@
+/* Class: CISC 3130
+ * Section: MY9, TY2, TY9
+ * EmplId:
+ * Name: Professor Katherine Chuang
+ */
+
+ // Imports go towards top of the file
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+class GreatExpectations extends Thread
+{
+  // Java classes can have private members that are not accessible outside the class
+  private int chapters = 0;
+
+  // Java members can be final, meaning they can't be changed
+  final static String data = "../data/great_expectations.txt";
+
+  // this is a default constructor that gets called when an object is created
+  public GreatExpectations()
+  {
+    System.out.println("Constructor called. Parsing this file: " + data);
+  }
+
+  // getter method to retrieve the private class member from another class
+  int getGreatExpectations()
+  {
+    return this.chapters;
+  }
+
+  // setter method to set the private class member
+  void setGreatExpectations(int i)
+  {
+    this.chapters = i;
+  }
+
+  // implementation in its own function with default access modifier to the class
+  void openBookandStudy()
+  {
+    System.out.print(" starting method openBookandStudy ");
+    File file = new File(data);
+    int counter = 0;
+
+    // try-catch block to handle exceptions when opening and reading files
+    try
+    {
+      Scanner scanner = new Scanner(file);
+
+      while (scanner.hasNextLine())
+      {
+          String s = scanner.nextLine();
+          if (s.contains("Chapter"))
+          {
+            // increment the counter, this is the only necessary line in this if block
+            counter += 1;
+
+            // Thread sleeping is for cosmetic reasons as the . is printed to screen
+            Thread.sleep(300);
+            System.out.print(".");
+          }
+      }
+      // Good habit to close the scanner after done reading a file
+      scanner.close();
+
+      // Make thread sleet, for cosmetic reasons
+      Thread.sleep(100);
+
+    } catch (Exception e) {
+      System.out.println("uh oh!");
+    }
+
+    // Save the number of chapters counted to instance member so it can be accessed later
+    this.setGreatExpectations(counter);
+  }
+} // end class GreatExpectations
+
+// There can only be one public class in a Java file.
+// The public class name must match the file name.
+public class Main
+{
+
+  // Every public class has a main method.
+  // There can only be one main method.
+  public static void main(String[] args)
+  {
+    GreatExpectations thissemester = new GreatExpectations();
+    thissemester.openBookandStudy();
+
+    // Print to console w/o new line
+    System.out.print("Charles Dickens wrote ");
+    System.out.print(thissemester.getGreatExpectations());
+
+    // Print to console w/ new line
+    System.out.println(" chapters in his book Great Expectations.");
+  }
+}
